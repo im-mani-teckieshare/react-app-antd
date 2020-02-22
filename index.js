590354c6359b430c93734597ec4409a2e69c5e85
im-mani-teckieshare <teckieshare@gmail.com>
2020-02-22 12:38:11 +0530

    [react-app-antd ] - basic setup

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import 'antd/dist/antd.css';
 import './style.css';
+import App from './app';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
 
 render(<App />, document.getElementById('root'));
