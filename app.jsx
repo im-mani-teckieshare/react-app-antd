a25159f7b07380b8e552ec70c2de29ed42e83290
im-mani-teckieshare <teckieshare@gmail.com>
2020-02-22 13:09:01 +0530

    drawer handler implemented

--- app.jsx
+++ app.jsx
@@ -1,28 +1,44 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import { Drawer, Button } from 'antd';
+import { Drawer, Button,Icon } from 'antd';
 
 class App extends React.Component {
-  state = { visible: false };
+  state = { visible: false,right:'0px',icon:'menu-unfold' };
 
   showDrawer = () => {
     this.setState({
-      visible: true,
+      visible: !this.state.visible,
+      right: !this.state.visible ?'257px':'0px',
+      icon:'menu-fold'
     });
   };
 
   onClose = () => {
     this.setState({
       visible: false,
+      right:'0px',
+      icon:'menu-unfold'
     });
   };
 
   render() {
+    const buttonStyle = {position:"fixed",right:this.state.right,
+    width: "30px",
+    height: "60px",
+    justifyContent: "center",
+    display: "flex",
+    background: "white",
+    color: "black",
+    borderColor: "#cccbcb",
+    boxShadow: "rgba(0, 0, 0, 0.15) -2px 0px 8px",
+    border: "none",
+    zIndex: "1001",
+    top:'50%'}
     return (
       <div
         style={{
-          height: 200,
+          height: '100vh',
           overflow: 'hidden',
           position: 'relative',
           border: '1px solid #ebedf0',
@@ -34,8 +50,8 @@ class App extends React.Component {
       >
         Render in this
         <div style={{ marginTop: 16 }}>
-          <Button type="primary" onClick={this.showDrawer}>
-            Open
+          <Button style={buttonStyle} type="primary" onClick={this.showDrawer}>
+           <Icon type={this.state.icon} />
           </Button>
         </div>
         <Drawer
