38aef23a399e9ce5fd726c43aaeaa477b9bd42ef
im-mani-teckieshare <teckieshare@gmail.com>
2020-02-23 18:04:47 +0530

    1 file modified

--- listContainer.jsx
+++ listContainer.jsx
@@ -1,57 +1,99 @@
-import React,{Component} from 'react';
-import InfiniteScroll from 'react-infinite-scroller'; 
-import {List ,Card,Spin} from "antd";
+import React, { Component } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { List, Card, Spin } from "antd";
 
-class ListContainer extends Component{
-
-render(){
-
-  const data = [
+const data = [
   {
-    title: 'Title 1',
+    title: "Title 1"
   },
   {
-    title: 'Title 2',
+    title: "Title 2"
   },
   {
-    title: 'Title 3',
+    title: "Title 3"
   },
   {
-    title: 'Title 4',
+    title: "Title 4"
   },
   {
-    title: 'Title 5',
+    title: "Title 5"
   },
   {
-    title: 'Title 6',
-  },
+    title: "Title 6"
+  }
 ];
 
-return (<InfiniteScroll
-    pageStart={0}
-    loadMore={loadFunc}
-    hasMore={true || false}
-    loader={ <Spin size="large" />}
->
-  <List
-    grid={{
-      gutter: 16,
-      xs: 1,
-      sm: 2,
-      md: 1,
-      lg: 1,
-      xl: 1,
-      xxl: 1,
-    }}
-    dataSource={data}
-    renderItem={item => (
-      <List.Item>
-        <Card title={item.title}>Card content</Card>
-      </List.Item>
-    )}
-  />
-</InfiniteScroll>)
-}
+class ListContainer extends Component {
+  state = {
+    list: [
+      {
+        title: "Title 1"
+      },
+      {
+        title: "Title 2"
+      },
+      {
+        title: "Title 3"
+      },
+      {
+        title: "Title 4"
+      },
+      {
+        title: "Title 5"
+      },
+      {
+        title: "Title 6"
+      }
+    ],
+    loading: false,
+    hasMore: true
+  };
+
+  loadFunc = () => {
+    console.log('comes.....')
+    alert()
+    this.setState({
+      loading: true
+    });
+
+    let newList = this.state.list.concat(...data);
+    console.log(newList);
+
+      this.setState({
+        list: newList,
+        loading: false
+      })
+  };
+  render() {
+    return (
+      <InfiniteScroll
+        pageStart={0}
+        initialLoad={false}
+        hasMore={false || !this.state.loading}
+        loadMore={this.loadFunc}
+        loader={<Spin size="large" />}
+       
+      >
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 1,
+            lg: 1,
+            xl: 1,
+            xxl: 1
+          }}
+          dataSource={this.state.list}
+          renderItem={item => (
+            <List.Item>
+              <Card title={item.title}>Card content</Card>
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
+    );
+  }
 }
 
 export default ListContainer;
